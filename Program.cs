750d66d095c2cdded96f1ea42f6ebf629b5f73b7
jonataferreira <ferreira.jonata@gmail.com>
2025-02-06 15:28:43 -0300

    Commit com exercícios sobre exceções

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+try
+{
+    string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+    foreach (string linha in linhas)
+    {
+        Console.WriteLine(linha);
+    }
+}
+catch (DirectoryNotFoundException ex)
+{
+    Console.WriteLine($"Diretório não encontrado {ex.Message}");
+}
+
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine($"Ocorreu um erro na leitura do arquivo {ex.Message}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu uma exceção genérica {ex.Message}");
+}
+
+
+
+
+
+
